4562f3e98764e5b055111130c05d030f4d2c73f8
YutoYamauchi <yuto.yamayama7915@gmail.com>
2017-04-17 12:45:54 +0900

    stop,startボタン設置

--- Beacon/ViewController.swift
+++ Beacon/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var scan: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startScan(_ sender: UIButton) {
+        if scan.titleLabel?.text == "start" {
+            scan.setTitle("stop", for: UIControlState.normal)
+        } else {
+            scan.setTitle("start", for: UIControlState.normal)
+        }
     }
 
 
